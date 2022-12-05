89a501db9c339e40b66b065f8ce2c51b55766371
hakan <hakan@DESKTOP-7IJG156.local>
2022-12-06 00:51:19 +0300

    10 - Add Cors

--- BookStoreApp/BookStoreApp.API/Program.cs
+++ BookStoreApp/BookStoreApp.API/Program.cs
@@ -13,6 +13,11 @@ builder.Host.UseSerilog((ctx,lc) =>
     lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration)
 );
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", b => b.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +28,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
